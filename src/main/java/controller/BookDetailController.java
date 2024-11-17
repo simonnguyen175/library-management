06543fe6ccf68faf0d387b90526cfc654ce656f5
simonnguyen175 <nguyensymanh35@gmail.com>
2024-11-17 22:50:01 +0700

    resolve conflict and add new update for BookDetail

---
+++ src/main/java/controller/BookDetailController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class BookDetailController {
+    @FXML
+    private Label bookTitleLabel;
+    @FXML
+    private Button backButton;
+    @FXML
+    private AnchorPane rootPane;
+
+    public void setBookTitle(String title) {
+        bookTitleLabel.setText(title);
+    }
+
+    @FXML
+    private void initialize() {
+        backButton.setOnAction(event -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Books.fxml"));
+                AnchorPane booksPane = loader.load();
+                rootPane.getChildren().setAll(booksPane);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
