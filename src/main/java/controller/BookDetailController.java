590e89b3e0878d1780dd3e107bf694605838c04f
LongNguyen2k5 <long20122005@gmail.com>
2024-11-22 00:16:45 +0700

    using call API to getBookInfo(with title), getDescription (with title) and getBook (with ISBN)

--- src/main/java/controller/BookDetailController.java
+++ src/main/java/controller/BookDetailController.java
@@ -1,23 +1,11 @@
 package controller;
 
-import javafx.application.Platform;
-import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import library.Book;
-
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
 
 public class BookDetailController {
     @FXML
