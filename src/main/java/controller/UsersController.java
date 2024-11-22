11f38a1e1553de6fc854a40fa703f94f90a5e1ee
simonnguyen175 <nguyensymanh35@gmail.com>
2024-11-23 04:35:35 +0700

    update search user feature

--- src/main/java/controller/UsersController.java
+++ src/main/java/controller/UsersController.java
@@ -6,10 +6,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import library.User;
@@ -37,6 +34,24 @@ public class UsersController extends Controller implements Initializable {
     @FXML
     private TableColumn<User, Void> deleteColumn;
 
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private Label emailLabel;
+
+    @FXML
+    private Label phoneLabel;
+
+    @FXML
+    private Label totalBooksBorrowedLabel;
+
+    @FXML
+    private Label totalBooksCurrentlyBorrowedLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         userIdColumn.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getUserId()).asObject());
@@ -47,6 +62,12 @@ public class UsersController extends Controller implements Initializable {
 
         // Load user data from the database
         userTable.setItems(getUserData());
+
+        searchTextField.setOnAction(event -> searchUser());
+        searchButton.setOnAction(event -> searchUser());
+
+        // Add listener to table selection
+        userTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showUserDetails(newValue));
     }
 
     private void addDeleteButtonToTable() {
@@ -73,20 +94,64 @@ public class UsersController extends Controller implements Initializable {
 
     private ObservableList<User> getUserData() {
         ObservableList<User> users = FXCollections.observableArrayList();
-        String query = "SELECT user_id, fullname, username FROM users;";
+        String query = "SELECT * FROM users;";
 
-        try (Statement stmt = connection.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
-            while (rs.next()) {
-                users.add(new User(
-                        rs.getInt("user_id"),
-                        rs.getString("fullname"),
-                        rs.getString("username")
-                ));
+        Thread dbThread = new Thread(() -> {
+            try (Statement stmt = connection.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
+                while (rs.next()) {
+                    users.add(new User(
+                            rs.getInt("user_id"),
+                            rs.getString("fullname"),
+                            rs.getString("username"),
+                            rs.getString("phone"),
+                            rs.getString("email")
+                    ));
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        });
+        dbThread.start();
 
         return users;
+    }
+
+    private void searchUser() {
+        String searchText = searchTextField.getText().trim();
+        if (!searchText.isEmpty()) {
+            String query = "SELECT user_id, fullname, username, phone, email FROM users WHERE user_id LIKE '%" + searchText + "%' OR fullname LIKE '%" + searchText + "%';";
+
+            Thread dbThread = new Thread(() -> {
+                ObservableList<User> users = FXCollections.observableArrayList();
+                try (Statement stmt = connection.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
+                    while (rs.next()) {
+                        users.add(new User(
+                                rs.getInt("user_id"),
+                                rs.getString("fullname"),
+                                rs.getString("username"),
+                                rs.getString("phone"),
+                                rs.getString("email")
+                        ));
+                    }
+                    userTable.setItems(users);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            });
+            dbThread.start();
+        }
+    }
+
+    private void showUserDetails(User user) {
+        if (user != null) {
+            System.out.println("User selected: " + user.getEmail());
+            emailLabel.setText("Email: " + user.getEmail());
+            phoneLabel.setText("SĐT: " + user.getPhone());
+        } else {
+            emailLabel.setText("");
+            phoneLabel.setText("");
+            totalBooksBorrowedLabel.setText("");
+            totalBooksCurrentlyBorrowedLabel.setText("");
+        }
     }
 }
