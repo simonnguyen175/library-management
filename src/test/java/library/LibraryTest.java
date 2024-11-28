d70d3d6277a9601d4d124ea903268873b8ce140c
nqmcl2735 <minhk17adtd@gmail.com>
2024-11-29 01:03:41 +0700

    Update w/ test by junit
    Using junit test func in Library

--- src/test/java/library/LibraryTest.java
+++ src/test/java/library/LibraryTest.java
@@ -2,7 +2,10 @@ package library;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import controller.BorrowBook;
 import controller.Controller;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Date;
 import main.LibraryManagement;
 import org.junit.jupiter.api.Test;
@@ -10,11 +13,49 @@ import org.junit.jupiter.api.Test;
 class LibraryTest {
 
   @Test
-  void addComment() {
-
+  void addComment(){
+    try {
+      String url = "jdbc:mysql://localhost:3306/library";
+      String dbUser = "root";
+      String dbPassword = "0";
+      Controller.connection = DriverManager.getConnection(url, dbUser, dbPassword);
+      Library lb = Library.getInstance();
+      Comment comment = new Comment(0, 240017, 1001, "Tuyet day", new Date());
+      lb.addComment(comment);
+    } catch (Exception e)
+    {
+      e.printStackTrace();
+    }
   }
 
   @Test
   void borrowBook() {
+    try {
+      String url = "jdbc:mysql://localhost:3306/library";
+      String dbUser = "root";
+      String dbPassword = "0";
+      Controller.connection = DriverManager.getConnection(url, dbUser, dbPassword);
+      Library lb = Library.getInstance();
+      lb.BorrowBook(240008, 1001, 1, "2024-12-01");
+    } catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+
+
+  @Test
+  void returnBook() {
+    try {
+      String url = "jdbc:mysql://localhost:3306/library";
+      String dbUser = "root";
+      String dbPassword = "0";
+      Controller.connection = DriverManager.getConnection(url, dbUser, dbPassword);
+      Library lb = Library.getInstance();
+      lb.ReturnBook(27);
+    } catch (Exception e)
+    {
+      e.printStackTrace();
+    }
   }
 }
