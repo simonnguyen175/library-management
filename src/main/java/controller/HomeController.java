5e98e8ce89a516803d51d1581e59764934140972
simonnguyen175 <nguyensymanh35@gmail.com>
2024-12-03 21:49:03 +0700

    dashboard UI upd

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -9,20 +9,7 @@ import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
-
-    @FXML
-    private Label dateLabel;
-
-    @FXML
-    private Label userCountLabel;
-
-    @FXML
-    private Label bookCountLabel;
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        dateLabel.setText("Date: " + LocalDate.now().toString());
-        userCountLabel.setText("Number of Users: 100"); // Replace with actual user count
-        bookCountLabel.setText("Number of Books: 500"); // Replace with actual book count
     }
 }
