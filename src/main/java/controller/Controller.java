938a064d9e93266471db8e8df4fdb6d17d1d229c
LongNguyen2k5 <long20122005@gmail.com>
2024-12-05 01:43:34 +0700

    CSS lor

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -12,9 +12,9 @@ public abstract class Controller {
     }
 
     private void connectToDatabase() {
-        String url = "jdbc:mysql://localhost:3306/library";
+        String url = "jdbc:mysql://localhost:3307/library";
         String dbUser = "root";
-        String dbPassword = "";
+        String dbPassword = "long20122005";
 
         try {
             Thread dbThread = new Thread(() -> {
